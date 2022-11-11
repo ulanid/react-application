6da45c127e5c3745740a5040f5269e88d604a36b
ulanid <tyg84085@gmail.com>
2022-11-11 13:26:07 +0200

    create

---
+++ src/components/content/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+
+function Content(){
+    return(
+        <div className="block">
+        
+        </div>
+       
+    );
+}
+
+
+
+
+export default Content;
