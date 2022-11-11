b73d7942578d5a2010fa9c04608a1aef477b86be
ulanid <tyg84085@gmail.com>
2022-11-11 13:11:37 +0200

    create

---
+++ src/components/header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './style.css';
+
+
+function Header(){
+    return(
+        <div className="menu">
+        <ul className="menu-text">
+            <li>homepage</li>
+            <li>pages</li>
+            <li>elements</li>
+            <li>portfolio layouts</li>
+            <li>gallery layouts</li>
+            <li>link text</li>
+            <li>a very long link text</li>
+         </ul>
+        </div>
+       
+    );
+}
+
+
+
+
+export default Header;
