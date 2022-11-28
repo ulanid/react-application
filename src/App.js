3dfa9d40829a13fe872172cd8ac6619b0fcb0082
ulanid <tyg84085@gmail.com>
2022-11-28 14:34:44 +0200

    saving

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navigation from './components/navigation';
 import Content from './components/content';
+import About from './components/about';
+import PageNotFound from './components/404';
 
 function App() {
   return (
     <div className="App">
     <Navigation />
     <Content />
-      
+    <About />
+    <PageNotFound />
     </div>
   );
 }
