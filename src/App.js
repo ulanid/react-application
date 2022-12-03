099b78baee1143b7f2d1cb53c3f68c1f1508d410
ulanid <tyg84085@gmail.com>
2022-12-03 17:34:40 +0200

    saving

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import WidgetWeather from './components/widget-weather';
 import './App.css';
+import { useState } from 'react';
 
+function App () {
+  const [cityName, setCityName] = useState();
+  const onBlur = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    setCityName(value)
+  };
+}
 
 
 function App() {
   return (
     <div className="App">
-     
-      
+    <div>
+      <ul id="growing-search-freebie">
+        <li>
+          <div class="growing-search">
+            <div class="input">
+              <input
+                onBlur={onBlur}
+                type="text"
+                name="search"
+                placeholder="Type your city"
+              />
+            </div><div class="submit">
+              <button type="submit" name="go_search">
+                <span class="fa fa-search"></span>
+              </button>
+            </div>
+          </div>
+        </li>
+      </ul>
     </div>
+    <WidgetWeather city={cityName} />
+  </div>
   );
 }
 
---
+++ src/components/widget-weather/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import './style.css';
+// https://neumorphism.io/#212130
+
+function WidgetWeather (props) {
+ const [isLoad, setIsLoad] = useState(true);
+ const [weatherData, setWeatherData] = useState();
+ useEffect(() => {
+  setIsLoad(true);
+  // https://openweathermap.org/current#current_JSON
+  axios.get(
+   `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&apiKey=6490ea3ba3c851128f51015075087ee2`
+  ).then((response) => {
+   console.log('response:', response.data);
+   setWeatherData(response.data);
+   setIsLoad(false);
+  });
+ }, [props.city]);
+
+ return (
+  <div className="widget-weather">
+   {
+    isLoad
+     ? (<div className="widget-weather-load-bar"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>)
+     : (<div><div>{props.city}</div>
+      <div className="widget-weather-coord">
+       <div>Longitude: {weatherData.coord.lon}</div>
+       <div>Latitude: {weatherData.coord.lat}</div>
+      </div></div>)
+   }
+  </div>
+ );
+}
+
+export default WidgetWeather;
