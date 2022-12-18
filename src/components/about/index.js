d897435d59364a0beaa1e5a006c772917118e57e
ulanid <tyg84085@gmail.com>
2022-12-18 20:44:24 +0200

    saved

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import image1 from '../imgs/woman.jpg';
 import New_about from './cards_about/index'
 
+
 let data = [
   {image: image1, title: "About me", description: "The rich text element allows you to create and format headings, paragraphs, blockquotes, images, and video all in one place instead of having to add and format them individually. Just double-click and easily create content.", title2: "Static and dynamic content editing", description2: "A rich text element can be used with static or dynamic content. For static content, just drop it into any page and begin editing. For dynamic content, add a rich text field to any collection and then connect a rich text element to that field in the settings panel. Voila!"}
 ]
