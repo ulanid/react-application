7a4f2c6ba4d4de96a6febf6c83a8ffc78aa16576
ulanid <116159219+ulanid@users.noreply.github.com>
2022-12-23 19:53:15 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
-import Header from './components/header';
+import React from 'react';
 
-function App () {
-  return(
-    <Header/>
-  )
+import Header from './Components/Header/Index';
+import Home from './Components/Home/index';
+import Paragraph2 from './Components/Paragraph2/index';
+import Projects from './Components/Projects/index';
+import Process from './Components/Process/index';
+import Testimonials from './Components/Testimonials/index';
+import Services from './Components/Services/index';
+import Pricing from './Components/Pricing/index';
+import Contact from './Components/Contact/index';
+import Footer from './Components/Footer/index';
+
+import './App.css';
+
+function App() {
+  return (
+     <div className='App' id='app'>
+        <Header />
+        <Home />
+        <Paragraph2 />
+        <Projects />
+        <Process />
+        <Testimonials />
+        <Services />
+        <Pricing />
+        <Contact />
+        <Footer />
+     </div>
+  );
 }
 
 export default App;
