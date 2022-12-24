e32163e1ce0319fedb92393be730ecc7f8013b65
ulanid <116159219+ulanid@users.noreply.github.com>
2022-12-24 10:13:03 +0200

    save

--- src/Components/Header/Index.jsx
+++ src/Components/Header/Index.jsx
@@ -7,6 +7,7 @@ function Header() {
         <div className={styles.navbar}>
           <div className={styles.menu}>
             <ul className={styles.ul}>
+
                <li><a className={styles.siteLogo}>Expert</a></li>
                <li className={styles.li}><a className={styles.a} href="#Home">Home</a></li>
                <li className={styles.li}><a className={styles.a} href="#Projects">Projects</a></li>
@@ -14,7 +15,8 @@ function Header() {
                <li className={styles.li}><a className={styles.a} href="#Testimonials">Testimonials</a></li>
                <li className={styles.li}><a className={styles.a} href="#Services">Services</a></li>
                <li className={styles.li2}><a className={styles.a} href="#Pricing">Pricing</a></li>  
-               <li className={styles.li3}><button className={styles.button}><a className={styles.a2} href="#Contact">Contact</a></button> </li> 
+               <li className={styles.li3}><button className={styles.button}>
+               <a className={styles.a2} href="#Contact">Contact</a></button> </li> 
             </ul>
           </div>
         </div>
